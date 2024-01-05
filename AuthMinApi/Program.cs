8921afe2a71f4055d29f3b43b072c053ca3350ca
aleks <aleks@email.noreply.com>
2024-01-05 17:38:30 +0500

    [AuthMinApi] Add claims check.

--- AuthMinApi/Program.cs
+++ AuthMinApi/Program.cs
@@ -21,15 +21,33 @@ app.UseHttpsRedirection();
 
 app.UseAuthentication();
 
-const string claimType = "usr";
-app.MapGet("/username", (HttpContext httpContext) =>
+const string userClaimType = "usr";
+app.MapGet("/unsecure", (HttpContext httpContext) => httpContext.User.FindFirstValue(userClaimType) ?? "empty");
+
+app.MapGet("/sweden", (HttpContext httpContext) =>
 {
-    return httpContext.User.FindFirstValue(claimType);
+    if (httpContext.User.Identities.All(id => id.AuthenticationType != authScheme))
+    {
+        httpContext.Response.StatusCode = StatusCodes.Status401Unauthorized;
+        return string.Empty;
+    }
+
+    if (!httpContext.User.HasClaim("passport_type", "eur"))
+    {
+        httpContext.Response.StatusCode = StatusCodes.Status403Forbidden;
+        return string.Empty;
+    }
+
+    return "allowed";
 });
 
 app.MapGet("/login", async (HttpContext httpContext) =>
 {
-    List<Claim> claims = [new Claim(claimType, "alex")];
+    List<Claim> claims =
+    [
+        new Claim(userClaimType, "alex"),
+        new Claim("passport_type", "eur")
+    ];
     ClaimsIdentity identity = new(claims, authScheme);
     var user = new ClaimsPrincipal(identity);
 
