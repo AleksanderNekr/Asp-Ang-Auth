5969886b1da4dfa4ca69c037876f879c5e444e04
aleks <aleks@email.noreply.com>
2024-01-05 06:10:54 +0500

    [OneCustFront] Add buttons to test endpoints.

--- ApiDomain/FrontDomain/src/app/app.component.ts
+++ ApiDomain/FrontDomain/src/app/app.component.ts
@@ -5,10 +5,13 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [ CommonModule, RouterOutlet ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'FrontDomain';
+
+  login = () => fetch("/api/login", { method: "post" })
+  testEndpoint = () => fetch("/api/test")
 }
