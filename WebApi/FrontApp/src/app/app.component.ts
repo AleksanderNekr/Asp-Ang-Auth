430e3d228d2f9c50466a8cc32680220371984891
aleks <aleks@email.noreply.com>
2024-01-05 09:12:30 +0500

    [OneCustFront] Add API calls.

--- WebApi/FrontApp/src/app/app.component.ts
+++ WebApi/FrontApp/src/app/app.component.ts
@@ -5,10 +5,16 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+    imports: [ CommonModule, RouterOutlet ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'FrontApp';
+
+  constructor() {
+  }
+
+  login = () => fetch("https://localhost:7234/api/login", { method: "post", credentials: "include" });
+  testEndpoints = () => fetch("https://localhost:7234/api/test", { method: "get", credentials: "include" });
 }
