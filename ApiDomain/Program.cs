0b498bc071e12b0e679687d1a9e575a92c5e1896
aleks <aleks@email.noreply.com>
2024-01-05 05:09:10 +0500

    [OneCustApi] Add standard secret and login endpoints.

--- ApiDomain/Program.cs
+++ ApiDomain/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+const string authScheme = "def";
+builder.Services
+    .AddAuthentication(authScheme)
+    .AddCookie(authScheme);
+
+builder.Services.AddAuthorization();
 
 WebApplication app = builder.Build();
 
@@ -12,5 +21,27 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+RouteGroupBuilder api = app.MapGroup("/api");
+api.MapGet("/test", () => "secret")
+    .RequireAuthorization()
+    .WithOpenApi();
+
+api.MapPost("/login", async context =>
+{
+    await context.SignInAsync(authScheme, new ClaimsPrincipal(
+            new ClaimsIdentity(
+                new Claim[]
+                {
+                    new(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString())
+                })),
+        new AuthenticationProperties
+        {
+            IsPersistent = true
+        });
+}).WithOpenApi();
 
 app.Run();
