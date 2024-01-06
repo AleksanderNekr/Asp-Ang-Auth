50f79c819ad175a7775396f19d6d333ede7706d9
aleks <aleks@email.noreply.com>
2024-01-06 17:38:58 +0500

    [AuthMinApi] Add custom auth schemas with external oauth.

--- AuthMinApi/Program.cs
+++ AuthMinApi/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.Options;
 using System.Security.Claims;
+using System.Text.Encodings.Web;
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
@@ -7,18 +10,51 @@ builder.Services.AddSwaggerGen();
 
 const string? authScheme = "cookie";
 const string? authScheme2 = "cookie2";
+const string localAuthScheme = "local";
+const string visitorAuthScheme = "visitor";
+const string patreonAuthScheme = "patreon-cookie";
 builder.Services.AddAuthentication(authScheme)
     .AddCookie(authScheme)
-    .AddCookie(authScheme2);
+    .AddCookie(authScheme2)
+    .AddCookie(localAuthScheme)
+    .AddCookie(patreonAuthScheme)
+    .AddScheme<CookieAuthenticationOptions, VisitorAuthHandler>(visitorAuthScheme, _ => {})
+    .AddOAuth("external-patreon", o =>
+    {
+        o.SignInScheme = patreonAuthScheme;
 
-const string policyName = "eu passport";
+        o.ClientId = "id";
+        o.ClientSecret = "secret";
+
+        o.AuthorizationEndpoint = "https://oauth.wiremockapi.cloud/oauth/authorize";
+        o.TokenEndpoint = "https://oauth.wiremockapi.cloud/oauth/token";
+        o.UserInformationEndpoint = "https://oauth.wiremockapi.cloud/userinfo";
+
+        o.CallbackPath = "/cb-patreon";
+
+        o.Scope.Add("profile");
+        o.SaveTokens = true;
+    });
+
+const string euPolicyName = "eu passport";
+const string customerPolicyName = "customer";
 builder.Services
     .AddAuthorizationBuilder()
-    .AddPolicy(policyName, policyBuilder =>
+    .AddPolicy(euPolicyName, policyBuilder =>
     {
         policyBuilder.RequireAuthenticatedUser()
             .AddAuthenticationSchemes(authScheme)
             .RequireClaim("passport_type", "eur");
+    })
+    .AddPolicy(customerPolicyName, policyBuilder =>
+    {
+        policyBuilder.RequireAuthenticatedUser()
+            .AddAuthenticationSchemes(localAuthScheme, visitorAuthScheme, patreonAuthScheme);
+    })
+    .AddPolicy("user", policyBuilder =>
+    {
+        policyBuilder.RequireAuthenticatedUser()
+            .AddAuthenticationSchemes(localAuthScheme);
     });
 
 WebApplication app = builder.Build();
@@ -45,7 +81,7 @@ app.MapGet("/denmark", (HttpContext httpContext) =>
 app.MapGet("/norway", (HttpContext httpContext) =>
 {
     return "allowed";
-}).RequireAuthorization(policyName);
+}).RequireAuthorization(euPolicyName);
 
 app.MapGet("/login", async (HttpContext httpContext) =>
 {
@@ -61,4 +97,45 @@ app.MapGet("/login", async (HttpContext httpContext) =>
     return "ok";
 }).AllowAnonymous();
 
+app.MapGet("/", context => Task.FromResult("Hello World!"))
+    .RequireAuthorization(customerPolicyName);
+
+app.MapGet("/login-local", async context =>
+{
+    List<Claim> claims = [new Claim(userClaimType, "Alex")];
+    ClaimsIdentity identity = new(claims, localAuthScheme);
+    ClaimsPrincipal user = new(identity);
+
+    await context.SignInAsync(localAuthScheme, user);
+});
+
+app.MapGet("/login-patreon", async context =>
+    await context.ChallengeAsync("external-patreon", new AuthenticationProperties
+    {
+        RedirectUri = "/"
+    })).RequireAuthorization("user");
+
 app.Run();
+
+public class VisitorAuthHandler(
+    IOptionsMonitor<CookieAuthenticationOptions> options,
+    ILoggerFactory logger,
+    UrlEncoder encoder) : CookieAuthenticationHandler(options, logger, encoder)
+{
+    protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        AuthenticateResult result = await base.HandleAuthenticateAsync();
+        if (result.Succeeded)
+        {
+            return result;
+        }
+
+        List<Claim> claims = [new Claim("usr", "Alex")];
+        ClaimsIdentity identity = new(claims, "visitor");
+        ClaimsPrincipal user = new(identity);
+
+        await Context.SignInAsync("visitor", user);
+
+        return AuthenticateResult.Success(new AuthenticationTicket(user, "visitor"));
+    }
+}
