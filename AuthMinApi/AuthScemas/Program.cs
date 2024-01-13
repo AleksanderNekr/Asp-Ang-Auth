a86046194ae23209d1113ac57daf5717da79a699
aleks <aleks@email.noreply.com>
2024-01-13 20:47:56 +0500

    [AuthSchemas] Add policies, mock oauth.

--- AuthMinApi/AuthScemas/Program.cs
+++ AuthMinApi/AuthScemas/Program.cs
@@ -11,11 +11,34 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddAuthentication()
     .AddCookie("local")
-    .AddScheme<CookieAuthenticationOptions, VisitorAuthHandler>("visitor", _ => {});
+    .AddScheme<CookieAuthenticationOptions, VisitorAuthHandler>("visitor", _ => {})
+    .AddCookie("patreon-cookie")
+    .AddOAuth("external-patreon", options =>
+    {
+        options.AuthorizationEndpoint = "https://oauth.wiremockapi.cloud/oauth/authorize";
+        options.TokenEndpoint = "https://oauth.wiremockapi.cloud/oauth/token";
+        options.UserInformationEndpoint = "https://oauth.wiremockapi.cloud/userinfo";
+
+        options.ClientId = "test-oauth-app";
+        options.ClientSecret = "123qweasdrty";
+
+        options.SaveTokens = true;
+
+        options.Scope.Add("profile");
+
+        options.CallbackPath = "/oauth-cb-patreon";
+        options.SignInScheme = "patreon-cookie";
+    });
+
 builder.Services.AddAuthorizationBuilder()
     .AddPolicy("customer", policyBuilder =>
     {
-        policyBuilder.AddAuthenticationSchemes("local", "visitor")
+        policyBuilder.AddAuthenticationSchemes("patreon-cookie", "local", "visitor")
+            .RequireAuthenticatedUser();
+    })
+    .AddPolicy("user", policyBuilder =>
+    {
+        policyBuilder.AddAuthenticationSchemes("local")
             .RequireAuthenticatedUser();
     });
 
@@ -40,6 +63,11 @@ app.MapGet("/login-local", LoginLocal)
     .Produces(200)
     .Produces(400);
 
+app.MapGet("/login-patreon", LoginPatreon)
+    .Produces<string>()
+    .Produces(400)
+    .RequireAuthorization("user");
+
 app.UseHttpsRedirection();
 
 app.Run();
@@ -61,4 +89,9 @@ IResult LoginLocal(HttpContext context)
     return user.Identity?.IsAuthenticated ?? false
         ? Results.Ok()
         : Results.BadRequest();
+}
+
+IResult LoginPatreon(HttpContext context)
+{
+    return Results.Challenge(new AuthenticationProperties { RedirectUri = "/" }, [ "external-patreon" ]);
 }
